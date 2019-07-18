78abfdb38029212cc75805f795026bc395f67eff
Janek <jankolanowski@gmail.com>
2019-07-18 22:11:00 +0200

    second commit

--- src/main/java/com/example/helloworld/RestApp/FileDownloadController.java
+++ src/main/java/com/example/helloworld/RestApp/FileDownloadController.java
@@ -1,22 +1,18 @@
 package com.example.helloworld.RestApp;
 
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @Controller
 public class FileDownloadController {
 	@RequestMapping(value = "/files/{file_name}", method =  RequestMethod.GET)
-	public void getFile(@PathVariable("file_name") String fileName, HttpServletResponse response) throws FileNotFoundException {
-		File initialFile = new File("src/main/com/example/helloworld/RestApp/sample.pdf");
-		InputStream targetStream = new FileInputStream(initialFile);
-
+	@ResponseBody
+	public FileSystemResource getFile(@PathVariable("file_name") String fileName){
+		return new FileSystemResource("C:\\Users\\Janek\\IdeaProjects\\RestApp\\src\\main\\resources\\sample.pdf");
 	}
 }
