4c3e6cc8bb564cc5a1feb05362b464b10fe64642
Antek <antonik68@gmail.com>
2019-07-18 22:37:32 +0200

    Anteks guide to maybe finishing this project

--- src/main/java/com/example/helloworld/RestApp/FileDownloadController.java
+++ src/main/java/com/example/helloworld/RestApp/FileDownloadController.java
@@ -2,17 +2,15 @@ package com.example.helloworld.RestApp;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class FileDownloadController {
-	@RequestMapping(value = "/files/{file_name}", method =  RequestMethod.GET)
+	@RequestMapping(value = "/files/", method =  RequestMethod.GET)
 	@ResponseBody
-	public FileSystemResource getFile(@PathVariable("file_name") String fileName){
-		return new FileSystemResource("C:\\Users\\Janek\\IdeaProjects\\RestApp\\src\\main\\resources\\sample.pdf");
+	public FileSystemResource getFile(@RequestParam("pierwszy_dekret") String pierwszyDekret){
+		//iText
+		return new FileSystemResource("C:\\Users\\Bracia\\IdeaProjects\\dekretator\\src\\main\\resources\\sample.pdf");
 	}
 }
--- src/main/java/com/example/helloworld/RestApp/RestAppApplication.java
+++ src/main/java/com/example/helloworld/RestApp/RestAppApplication.java
@@ -10,4 +10,21 @@ public class RestAppApplication {
 		SpringApplication.run(RestAppApplication.class, args);
 	}
 
+	//Stronkę w thymeleafie z odpowiednią formą
+	//żeby dodać iText dodaj do Poma odpowiednią zależność
+	/*
+	<!-- https://mvnrepository.com/artifact/com.itextpdf/itextpdf -->
+		<dependency>
+		    <groupId>com.itextpdf</groupId>
+		    <artifactId>itextpdf</artifactId>
+		    <version>5.5.13.1</version>
+		</dependency>
+
+	 */
+	//https://www.tutorialspoint.com/itext/itext_overview.htm
+	//jeżeli z iTextem będą problemy to korzystałem kiedyś z JasperReports i to jakoś działało
+	//można tez użyć dowolnego programu i odpalać go z command line-a (tylko pamiętaj, że serwer będzie na linuxie)
+	//jest tez opcja generacji wszystkiego po froncie https://stackoverflow.com/questions/742271/generating-pdf-files-with-javascript
+	//na jsie byłoby o tyle fajnie, że nie potrzebujesz żadnego serwera - tylko static content
+
 }
