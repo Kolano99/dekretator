eb8ade058bd4dacc98fdb53f4dd14fb6aa5e8dfe
Janek <jankolanowski@gmail.com>
2019-07-20 08:49:49 +0200

    First try with thymeleaf

--- src/main/java/com/example/helloworld/RestApp/FileDownloadController.java
+++ src/main/java/com/example/helloworld/RestApp/FileDownloadController.java
@@ -2,17 +2,24 @@ package com.example.helloworld.RestApp;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 
+/*@Controller
+public class FileDownloadController {
+	@RequestMapping(value = "/files/", method =  RequestMethod.GET)
+	@ResponseBody
+	public FileSystemResource getFile(@RequestParam("pierwszy_dekret") String pierwszyDekret){
+		//iText
+		return new FileSystemResource("C:\\Users\\Bracia\\IdeaProjects\\dekretator\\src\\main\\resources\\sample.pdf");
+	}
+}*/
 @Controller
 public class FileDownloadController {
-	@RequestMapping(value = "/files/{file_name}", method =  RequestMethod.GET)
-	@ResponseBody
-	public FileSystemResource getFile(@PathVariable("file_name") String fileName){
-		return new FileSystemResource("C:\\Users\\Janek\\IdeaProjects\\RestApp\\src\\main\\resources\\sample.pdf");
+	@GetMapping(value = "/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
 	}
 }
